7e4dc54f10d11d5cd19288a7cff258bf3d8478d6
rkb429 <rkb429@naver.com>
2018-06-29 15:34:22 +0900

    모든아이피허용

--- firstServer/firstServer/settings.py
+++ firstServer/firstServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l57$a7mr$kf#a%b=r@%bmdul8(38=4)a%5!a)op^*9)zs&9b=j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
