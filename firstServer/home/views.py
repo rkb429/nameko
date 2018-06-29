6c3d7ebdc97d3deca9f8402af29c5c6c68eb32c1
rkb429 <rkb429@naver.com>
2018-06-29 16:47:02 +0900

    버튼1추가

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from bs4 import BeautifulSoup
+import urllib.request
+import json
+import datetime
+import requests
 def keyboard(request):
     return JsonResponse(
         {
