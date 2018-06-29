cd2fcc6a477e846c0e7bf22033e09e0d8ee41b3c
rkb429 <rkb429@naver.com>
2018-06-29 16:08:04 +0900

    키보드추가

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
