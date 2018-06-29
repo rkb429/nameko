9386329cd00bcccac0d24f3818e58511cc0a1254
rkb429 <rkb429@naver.com>
2018-06-29 16:03:51 +0900

    키보드추가

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def keyboard(requast):
+    return JsonResponse(
+        {
+            "type" : "buttons",
+            "buttons" : ["선택 1", "선택 2", "선택 3"]
+        }   
+    )
