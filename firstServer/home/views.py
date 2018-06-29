0c0b4d64beafc2040574d1a9c3bd9ae096bb3d5e
rkb429 <rkb429@naver.com>
2018-06-29 16:39:47 +0900

    버튼1 추가

--- firstServer/home/views.py
+++ firstServer/home/views.py
@@ -10,3 +10,14 @@ def keyboard(request):
             "buttons" : ["선택 1", "선택 2", "선택 3"]
         }   
     )
+def message(request):
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content'] #버튼 항목중 무엇을 눌렀는가
+
+    if return_str == '선택 1':
+        return JsonResponse({ #return 밑에는 공통어
+            "message": {
+                "text": "안녕하세요"
+            }
+        })
